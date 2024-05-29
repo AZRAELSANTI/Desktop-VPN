aa0c6f72e5097a213444287d73898a5a884afdb0
kirill <racconrolly@gmail.com>
2024-05-29 22:27:34 +0300

    Добавьте файлы проекта.

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Guna2CircleButton1_Click(sender As Object, e As EventArgs) Handles Guna2CircleButton1.Click
+        If Not pn_menu.Visible Then
+            Guna2Transition1.ShowSync(pn_menu)
+        Else
+            Guna2Transition1.HideSync(pn_menu)
+        End If
+    End Sub
+
+    Private Sub Label22_Click(sender As Object, e As EventArgs) Handles Label22.Click
+
+    End Sub
+
+    Private Sub Label21_Click(sender As Object, e As EventArgs) Handles Label21.Click
+
+    End Sub
+End Class
